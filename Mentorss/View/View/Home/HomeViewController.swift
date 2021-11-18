30b0b8c758eb0dcd737d85519824502ac13c7594
Guboneui <starku2249@naver.com>
2021-11-19 04:55:39 +0900

    Add Home Base Design

--- Mentorss/View/View/Home/HomeViewController.swift
+++ Mentorss/View/View/Home/HomeViewController.swift
@@ -9,21 +9,42 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var showDetailStackView: UIStackView!
+    
+    @IBOutlet weak var findMyOwnMentorButton: UIButton!
+    @IBOutlet weak var challengeMentorButton: UIButton!
+    
+    
+    override func loadView() {
+        super.loadView()
+        configUI()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // 홈화면 UI 함수
+    
+    func configUI() {
+        findMyOwnMentorButton.layer.cornerRadius = 4
+        findMyOwnMentorButton.layer.shadowColor = UIColor(named: "primary800")?.cgColor
+        findMyOwnMentorButton.layer.shadowOffset = CGSize(width: 0, height: 1)
+        findMyOwnMentorButton.layer.shadowOpacity = 0.2
+        
+        
+        challengeMentorButton.layer.cornerRadius = 4
+        challengeMentorButton.layer.shadowColor = UIColor(named: "primary800")?.cgColor
+        challengeMentorButton.layer.shadowOffset = CGSize(width: 0, height: 1)
+        challengeMentorButton.layer.shadowOpacity = 0.2
+        
+        
     }
-    */
+
 
 }
