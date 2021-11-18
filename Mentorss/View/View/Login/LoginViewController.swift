fb8b52aa7fdf6448c11eb01264f1afee3a218f08
Guboneui <starku2249@naver.com>
2021-11-19 01:55:56 +0900

    Add move to home base logic

--- Mentorss/View/View/Login/LoginViewController.swift
+++ Mentorss/View/View/Login/LoginViewController.swift
@@ -15,15 +15,12 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+        let storyBoard = UIStoryboard(name: "Home", bundle: nil)
+        let homeVC = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        self.changeRootViewController(homeVC)
     }
-    */
+    
 
 }
